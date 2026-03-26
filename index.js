d22cb0b80c2a3c89feed3e5a5aa1dd93483b0027
sanwochmcbark-source <sanwochmcbark@gmail.com>
2026-03-26 09:21:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -329,6 +329,7 @@ if ('serviceWorker' in navigator) {
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
+
 app.listen(3000, () => {
   console.log("Running on http://localhost:3000");
 });
